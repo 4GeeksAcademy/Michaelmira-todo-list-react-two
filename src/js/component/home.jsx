dda0061a7116ee424b4971140816c132a2ad8f0e
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-09 18:23:17 +0000

    add Delete functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 function TodoItem({label, is_done, delete_todo, toggle_todo}){
 	return (
 		<div className="todo-item">
-			<input type="checkbox"checked={is_done} onClick={toggle_todo} />
+			<input type="checkbox"checked={is_done} onChange={toggle_todo} />
 			<span className="todo-text"> {label} </span>
 			<button className="btn btn-danger" onClick= {delete_todo}>
 				Delete
@@ -15,32 +15,44 @@ function TodoItem({label, is_done, delete_todo, toggle_todo}){
 
 //create your first component
 const Home = () => {
-	const [todos, setTodos] = useState([]);
-	const [item, setItem] = useState({
-		label: "give the cats some tuna cans.",
-		is_done: false,
-	});
+	const [todos, setTodos] = useState([
+		{label: "give the cats some tuna.", is_done: false},
+		{label: "Feed the cats", is_done: false},
+	]);
 
 	return (
 	<>
 		<div className="container-flex">
-			<form className="container d-flex flex-column align-items-center justify-content-start" >
+			<form 
+			onSubmit={(ev) => ev.preventDefault()}
+			className="container d-flex flex-column align-items-center justify-content-start" >
 				<h1>Todo List</h1>
 				<input class="form-control form-control-lg" 
 				type="text" 
 				placeholder="What do you want to get done today" 
 				aria-label="todo list input field">
 				</input>
-				<TodoItem 
-					label={item.label}
-					is_done={item.is_done}
-					toggle_todo={() => item.is_done}
-				/>
-				<TodoItem label = "feed the cats" is_done={false} />
-				<TodoItem label = "give cats attention" is_done={false} />
-				
-				
-				
+				{todos.map((item, idx) => (
+					<TodoItem 
+						key={idx}
+						label={item.label}
+						is_done={item.is_done}
+						toggle_todo={() => 
+							setTodos(
+								todos.toSpliced(idx, 1, {
+									label: item.label,
+									is_done: !item.is_done,
+								})
+							)
+						}
+						delete_todo={() => 
+							setTodos(
+								todos.toSpliced(idx, 1
+								)
+							)
+						}
+					/>
+				))}
 			</form>
 		</div>
 	</>
