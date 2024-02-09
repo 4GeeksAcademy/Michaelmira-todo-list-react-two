711f3e2ae89386b46df3c70520fd2e57c3de55bd
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-09 16:02:56 +0000

    add text style multiply todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,8 +7,32 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 	<>
-		<div className="text-center">
-			
+		<div className="container-flex">
+			<form className="container d-flex flex-column align-items-center justify-content-start" >
+				<h1>Todo List</h1>
+				<input class="form-control form-control-lg" 
+				type="text" 
+				placeholder="What do you want to get done today" 
+				aria-label="todo list input field">
+				</input>
+				<div className="todo-item">
+					<input type="checkbox"/>
+					<span className="todo-text"> feed the cat </span>
+					<div className="btn btn-danger">Delete</div>
+				</div>
+				<div className="todo-item">
+					<input type="checkbox"/>
+					<span className="todo-text"> feed the cat </span>
+					<div className="btn btn-danger">Delete</div>
+				</div>
+				<div className="todo-item">
+					<input type="checkbox"/>
+					<span className="todo-text"> feed the cat </span>
+					<div className="btn btn-danger">Delete</div>
+				</div>
+				
+				
+			</form>
 		</div>
 	</>
 	);
