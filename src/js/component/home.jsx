5d338c4da2a3ff00ac07b590712d94e054e37442
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-09 18:05:17 +0000

    create delete_todo and toggle_todo and functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+function TodoItem({label, is_done, delete_todo, toggle_todo}){
+	return (
+		<div className="todo-item">
+			<input type="checkbox"checked={is_done} onClick={toggle_todo} />
+			<span className="todo-text"> {label} </span>
+			<button className="btn btn-danger" onClick= {delete_todo}>
+				Delete
+			</button>
+		</div>
+	)
+}
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([]);
+	const [item, setItem] = useState({
+		label: "give the cats some tuna cans.",
+		is_done: false,
+	});
+
 	return (
 	<>
 		<div className="container-flex">
@@ -15,21 +31,14 @@ const Home = () => {
 				placeholder="What do you want to get done today" 
 				aria-label="todo list input field">
 				</input>
-				<div className="todo-item">
-					<input type="checkbox"/>
-					<span className="todo-text"> feed the cat </span>
-					<div className="btn btn-danger">Delete</div>
-				</div>
-				<div className="todo-item">
-					<input type="checkbox"/>
-					<span className="todo-text"> feed the cat </span>
-					<div className="btn btn-danger">Delete</div>
-				</div>
-				<div className="todo-item">
-					<input type="checkbox"/>
-					<span className="todo-text"> feed the cat </span>
-					<div className="btn btn-danger">Delete</div>
-				</div>
+				<TodoItem 
+					label={item.label}
+					is_done={item.is_done}
+					toggle_todo={() => item.is_done}
+				/>
+				<TodoItem label = "feed the cats" is_done={false} />
+				<TodoItem label = "give cats attention" is_done={false} />
+				
 				
 				
 			</form>
